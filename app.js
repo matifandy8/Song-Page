db4094fe8746953b8a6e77071de3eeb9f3ecb04b
matifandy <matifandy@gmail.com>
2020-09-04 19:32:23 -0300

    commit

--- app.js
+++ app.js
@@ -7,17 +7,16 @@ const d = document,
  $song = d.getElementById(".song");
 
  $form.addEventListener("submit",async e => {
-  e.preventDefault
+  e.preventDefault();
 
   try{
-    $loader.style.display = "block";
 
 
     let artist = e.target.artist.value.toLowerCase(),
         song = e.target.song.value.toLowerCase(),
         $artistTemplate = "",
         $songTemplate = "",
-        artistAPI = `http://theaudiodb.com/api/v1/json/1/search.php?s=${artist}`,
+        artistAPI = `https://theaudiodb.com/api/v1/json/1/search.php?s=${artist}`,
         songAPI = `https://api.lyrics.ovh/v1/${artist}/${song}`,
         artistFetch = fetch(artistAPI),
         songFetch = fetch(songAPI),
@@ -25,17 +24,43 @@ const d = document,
         artistData = await artistRes.json(),
         songData = await songRes.json();
 
+        console.log(artistRes, songRes);
+        console.log(artistData, songData);
+
         if(artistData.artists === null){
           $artistTemplate = `<h2>No existe el intérprete <mark>${artist}</mark></h2>`
         }else {
-            
+            let artist = artistData.artists[0];
+            $artistTemplate = `
+             <h2>${artist.strArtist}</h2>
+             <img src="${artist.strArtistThumb}" alt="${artist.strArtist}">
+             <p>
+               ${artist.intBornYear}-${artist.intDiedYear || "Presente"}
+             </p>
+             <p>${artist.strCountry}</p>
+             <p>${artist.strGenre}-${artist.strStyle}</p>
+             <a href="http://${artist.strWebsite}" target="_blank">Sitio Web</a>
+             <p>${artist.strBiographyEN}</p>
+            `;
         }
+
+        if(songData.error){
+          $songTemplate = `<h2>No existe la cancion <mark>${song}</mark> </h2>`;
+        }else{
+          $songTemplate = `
+          <h2>${song}</h2>
+          <blckqoute>${songData.lyrics}</blockqoute>
+          `;
+        }
+
+        $artist.innerHTML = $artistTemplate;
+        $song.innerHTML = $songTemplate;
 
 
   } catch (err){
     console.log(err);
-    $loader.style.display = "none";
+    
     let message = err.statusText || "Ocurrio un error";
-    $error.innerHTML = `<p>Error ${err.status}: ${message}</p>`
+    $error.innerHTML = `<p>Error ${err.status}:${message}</p>`;
   }
  });
